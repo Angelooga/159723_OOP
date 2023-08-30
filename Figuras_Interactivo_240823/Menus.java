850d001bdc94dc4bc2071025ac988239e25c41ad
Angelooga <141773267+Angelooga@users.noreply.github.com>
2023-08-29 19:07:04 -0600

    Add files via upload

--- Figuras_Interactivo_240823/Menus.java
+++ Figuras_Interactivo_240823/Menus.java
@@ -1,11 +1,14 @@
 package createFigures;
 
 public class Menus {
+	boolean flag;
 	
-	public void welcomeMenu() {
+	public void welcomeMenu(boolean flag) {
 		// This method prints the initial menu.
-		System.out.println("Welcome to the figure generator!\n");
-		System.out.println("Please, choose the figure you want to create.");
+		if(!flag) {
+			System.out.println("Welcome to the figure generator!");
+		}
+		System.out.println("\nPlease, choose the figure you want to create.");
 		System.out.println("1. Square\n" 
 				+ "2. Circle\n");
 	}
@@ -17,7 +20,7 @@ public class Menus {
 	public void askRadius() {
 		// This method prints the prompt to ask the user the circle radius.
 		System.out.println("\nYou chose Circle! "
-				+ "Please, introduce the radius of the circle radius.\n");
+				+ "Please, introduce the length of the circle radius.\n");
 	}
 	public void squareMenu() {
 		// This method prints the menu corresponding to the square characteristics.
@@ -30,6 +33,13 @@ public class Menus {
 		// This method prints the menu corresponding to the square characteristics.
 		System.out.println("\nWhat would you like to calculate?");
 		System.out.println("1. The Circle circumference\n"
-				+ "3. The Circle area\n");
+				+ "2. The Circle area\n");
+	}
+	public void askAgain() {
+		// This method prints the prompt to ask whether the user wants to create
+		// another figure.
+		System.out.println("\nWould you like to try again?\n"
+				+ "Press 1 to continue.\n"
+				+ "Press 0 to quit\n");
 	}
 }
--- Figuras_Interactivo_240823/Program.java
+++ Figuras_Interactivo_240823/Program.java
@@ -4,61 +4,85 @@ import java.util.Scanner;
 public class Program {
 	
 	public void executeProgram() {
+		// This flag indicates whether the loop will keep going or not
+		boolean flag = false;
 		
 		// Initializing the instances that will store the inputs
 		Scanner initialChoice = new Scanner(System.in);
 		Scanner figureParam = new Scanner(System.in);
 		Scanner calcChoice = new Scanner(System.in);
+		Scanner againChoice = new Scanner(System.in);
 		
-		// Printing the welcome menu
-		new Menus().welcomeMenu();
-		
-		// Obtaining the choice input from the user
-		String inChoice = initialChoice.nextLine();
-		int scannedChoice = Integer.parseInt(inChoice);
-		
-		if(scannedChoice == 1) {
+		do {
+			// Printing the welcome menu
+			// The parameter is used deploy or not the welcome prompt
+			new Menus().welcomeMenu(flag);
 			
-			// Printing the prompt that ask the user the square length
-			new Menus().askLength();
-			// Obtaining the length choice from the user
-			String figParam = figureParam.nextLine();
-			int scannedParam = Integer.parseInt(figParam);
+			// Obtaining the choice input from the user
+			String inChoice = initialChoice.nextLine();
+			int scannedChoice = Integer.parseInt(inChoice);
 			
-			// Printing the square menu
-			new Menus().squareMenu();
-			// Obtaining the decision from the user
-			String calChoice = calcChoice.nextLine();
-			int scannedCalc = Integer.parseInt(calChoice);
-
-			// Printing the desired parameter
-			Square mySquare = new Square((float)scannedParam, scannedCalc);
-			mySquare.printParam();
+			switch(scannedChoice){
+				case 1:{
+					// Printing the prompt that ask the user the square length
+					new Menus().askLength();
+					// Obtaining the length choice from the user
+					String figParam = figureParam.nextLine();
+					int scannedParam = Integer.parseInt(figParam);
+					
+					// Printing the square menu
+					new Menus().squareMenu();
+					// Obtaining the decision from the user
+					String calChoice = calcChoice.nextLine();
+					int scannedCalc = Integer.parseInt(calChoice);
+					
+					Square mySquare = new Square((float)scannedParam, scannedCalc);
+					mySquare.printParam();
+					break;
+				}
+				case 2:{
+					// Printing the prompt that ask the user the circle radius
+					new Menus().askRadius();
+					// Obtaining the length choice from the user
+					String figParam = figureParam.nextLine();
+					int scannedParam = Integer.parseInt(figParam);
+					
+					// Printing the circle menu
+					new Menus().circleMenu();
+					// Obtaining the decision from the user
+					String calChoice = calcChoice.nextLine();
+					int scannedCalc = Integer.parseInt(calChoice);
+					
+					Circle myCircle = new Circle((float)scannedParam, scannedCalc);
+					myCircle.printParam();
+					break;
+				}
+				default:{
+					System.out.println("Please, choose a valid option...");
+					break;
+				}
+			}
+			// Asking the user if he wants to repeat the process
+			new Menus().askAgain();
+			// Storing the user's input
+			String agaChoice = againChoice.nextLine();
+			int scanChoice = Integer.parseInt(agaChoice);
 			
-		}else {
+			// Changing the flag variable to see if loop should end or not
+			if(scanChoice == 1) {
+				flag = true;
+			}else {
+				flag = false;
+			}
 			
-			// Printing the prompt that ask the user the circle radius
-			new Menus().askRadius();
-			// Obtaining the length choice from the user
-			String figParam = figureParam.nextLine();
-			int scannedParam = Integer.parseInt(figParam);
-			
-			// Printing the circle menu
-			new Menus().circleMenu();
-			// Obtaining the decision from the user
-			String calChoice = calcChoice.nextLine();
-			int scannedCalc = Integer.parseInt(calChoice);
-
-			// Printing the desired parameter
-			Circle myCircle = new Circle((float)scannedParam, scannedCalc);
-			myCircle.printParam();
-			
-		}
+		}while(flag);
+		// Printing the gratitude message
+		System.out.println("Thanks for using UDLAP's figure generator! ");
 		
 		initialChoice.close();
 		figureParam.close();
 		calcChoice.close();
-		
+		againChoice.close();
 	}
 	
 }
